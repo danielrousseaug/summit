39156021c9aa8fceef4689609a9b21c797ed1c6d
Daniel Rousseau <danielrousseaug@gmail.com>
2025-09-25 12:06:44 -0600

    Integrate PDF.js for viewer

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
